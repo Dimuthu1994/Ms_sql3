5ed4cd6e689fd5854f02f0bc68fb49c732026c81
ddimuthu <148847546+ddimuthu@users.noreply.github.com>
2023-11-10 21:18:07 +0530

    users controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,15 +12,39 @@ public class UserController : ControllerBase
         _dapper = new DataContextDapper(config);
     }
 
-    // [HttpGet("GetUsers/{testValue}")]
-    // public IEnumerable<User> GetUsers(string testValue)
-    // {
-       
-    // }
-
     [HttpGet("TestConnection")]
     public DateTime TestConnection()
     {
         return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    }
+
+    [HttpGet("GetUsers")]
+    public IEnumerable<User> GetUsers()
+    {
+        string sql = @"SELECT [UserId],
+        [FirstName],
+        [LastName],
+        [Email],
+        [Gender],
+        [Active]
+        FROM TutorialAppSchema.Users";
+        IEnumerable<User> users = _dapper.LoadData<User>(sql);
+        return users;
+    }
+
+    [HttpGet("GetSingleUser/{userId}")]
+    public User GetSingleUser(int userId)
+    {
+        string sql = @"SELECT [UserId],
+        [FirstName],
+        [LastName],
+        [Email],
+        [Gender],
+        [Active]
+        FROM TutorialAppSchema.Users
+        WHERE UserId= "+userId.ToString();
+
+        User user = _dapper.LoadDataSingle<User>(sql);
+        return user;
     }
 }
