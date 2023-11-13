5689681ecd57e62edcea2b032569f072596fdee4
ddimuthu <148847546+ddimuthu@users.noreply.github.com>
2023-11-13 16:06:39 +0530

    auth setting and models

---
+++ Data/UserRepository.cs
@@ -0,0 +1,7 @@
+namespace DotnetAPI.Data
+{
+    public class UserRepository
+    {
+        
+    }
+}
---
+++ Dtos/UserForRegistrationDto.cs
@@ -0,0 +1,11 @@
+namespace DotnetAPI.Dtos
+{
+    partial class UserForRegistrationDto
+    {
+        string Email {get; set;} = "";
+        string Password {get; set;} = "";
+        
+        string PasswordConfirm {get; set;} = "";
+        
+    }
+}
